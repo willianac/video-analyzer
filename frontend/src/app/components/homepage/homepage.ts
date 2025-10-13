135554a8c37ee187bd4bb4f33cec46a7f559e583
Willian Almeida <willianac01@gmail.com>
2025-10-13 12:52:35 -0300

    feat: add animation lib

--- frontend/src/app/components/homepage/homepage.ts
+++ frontend/src/app/components/homepage/homepage.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { trigger, transition } from "@angular/animations";
+import { fadeIn } from "@ngverse/motion/animatecss"
 
 @Component({
   selector: 'app-homepage',
   imports: [NzButtonModule, NzIconModule, RouterLink],
   templateUrl: './homepage.html',
-  styleUrl: './homepage.css'
+  styleUrl: './homepage.css',
+  animations: [
+    trigger("show", [
+      transition(":enter", fadeIn())
+    ])
+  ]
 })
 export class Homepage {
 
--- frontend/src/app/core/auth/name/name.ts
+++ frontend/src/app/core/auth/name/name.ts
@@ -1,10 +1,19 @@
+import { trigger, transition } from '@angular/animations';
 import { Component } from '@angular/core';
+import { fadeIn } from '@ngverse/motion/animatecss';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-name',
-  imports: [],
+  imports: [NzInputModule, NzButtonModule],
   templateUrl: './name.html',
-  styleUrl: './name.css'
+  styleUrl: './name.css',
+   animations: [
+    trigger("show", [
+      transition(":enter", fadeIn())
+    ])
+  ]
 })
 export class Name {
 
