ef619fd7a63830d6fddb1cf6a5277b822337eb94
Willian Almeida <willianac01@gmail.com>
2025-10-14 14:51:19 -0300

    feat: handle request errors in frontend

--- frontend/src/app/components/analyzer/analyzer.ts
+++ frontend/src/app/components/analyzer/analyzer.ts
@@ -5,10 +5,11 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { Summary } from '../../services/summary';
 import { FormsModule } from '@angular/forms';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
 
 @Component({
   selector: 'app-analyzer',
-  imports: [NzInputModule, NzButtonModule, FormsModule],
+  imports: [NzInputModule, NzButtonModule, FormsModule, NzAlertModule],
   templateUrl: './analyzer.html',
   styleUrl: './analyzer.css',
   animations: [
@@ -22,18 +23,41 @@ export class Analyzer {
   loading = false
   videoId = ""
   summary = ""
+  summaryRequestErr = "";
+  showBlankInputErr = false
 
-  wait() {
+  public getSummary() {
+    if(!this.videoId) {
+      return this.showBlankInputErr = true;
+    }
     this.loading = true
     
-    this.summaryService.getSummary(this.videoId).subscribe({
+    return this.summaryService.getSummary(this.videoId).subscribe({
       next: (res) => {
         console.log(res)
         this.summary = res
+        this.loading = false;
       },
       error: (err) => {
-        console.log(err)
+        this.loading = false;
+        this.handleErrors(err)
       }
     })
+  }
+
+  private handleErrors(err: any) {
+    const errMessage = err.error as string;
+    if(err.status === 0) {
+      return this.summaryRequestErr = "Serviço indisponível no momento, por favor tente novamente mais tarde."
+    }
+    if(errMessage.startsWith("Error: Video is too long")) {
+      return this.summaryRequestErr = "Vídeo muito grande. É permitido usar vídeos de até 1 minuto (60 segundos)"
+    }
+    if(errMessage.startsWith("Error: Failed to download video.")) {
+      return this.summaryRequestErr = "Não foi possível usar este vídeo. Pode ser que o ID esteja errado ou que esteja indisponivel para baixar."
+    }
+
+    console.log(err.error)
+    return null
   }
 }
