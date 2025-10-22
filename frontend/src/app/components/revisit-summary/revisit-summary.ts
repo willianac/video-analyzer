5b9a765e8636c135114048a08926c13b74d27ab9
Willian Almeida <willianac01@gmail.com>
2025-10-22 13:45:18 -0300

    feat: add fade animation to analyzer/revisit-summary pages

--- frontend/src/app/components/revisit-summary/revisit-summary.ts
+++ frontend/src/app/components/revisit-summary/revisit-summary.ts
@@ -2,12 +2,19 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { Summary } from '../../core/types/summary';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { trigger, transition } from '@angular/animations';
+import { fadeIn } from '@ngverse/motion/animatecss';
 
 @Component({
   selector: 'app-revisit-summary',
   imports: [NzButtonModule, RouterLink],
   templateUrl: './revisit-summary.html',
-  styleUrl: './revisit-summary.css'
+  styleUrl: './revisit-summary.css',
+  animations: [
+    trigger("show", [
+      transition(":enter", fadeIn())
+    ])
+  ]
 })
 export class RevisitSummary {
   router = inject(Router);
